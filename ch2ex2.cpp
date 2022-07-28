b687a1aaff2971007cc67d9d8522e916db1f7190
Deepak kumar jha <44145322+kumardeepak99@users.noreply.github.com>
2022-07-29 01:01:33 +0530

    Create ch2ex2.cpp
    average of two numbers

---
+++ ch2ex2.cpp
@@ -0,0 +1,46 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    float num1,num2,sum,avg;
+    
+    cout<<"Enter two numbers\n";    
+    
+    cin>>num1>>num2;
+    sum=num1+num2;
+    avg=sum/2;
+    
+    cout<<"average of"<<num1<<" , "<<num2<<" is ="<<avg<<endl;
+
+    return 0;
+    
+        /*  
+        descriptions
+        
+        variable can be any single type of data container to hold the value
+        
+        if integer we say type integer
+        
+        if very big integer we say long or long long
+        
+        if decimal we say float
+        
+        if big ecimal we say double
+        
+        if binary true/false we say bool etc..
+        
+        
+        cin>>var1>>var2 ...    reading multiple variable 
+        
+        similarly using cout<<string<<num1<<expressions ....
+        
+        multiple input or output we can do in a single shot
+        
+        
+        this is called operator cascaing in c++
+    
+    */
+    
+}
