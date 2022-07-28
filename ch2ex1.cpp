fd599f9d9bf11b513580389bb74d5f325e662d5c
Deepak kumar jha <44145322+kumardeepak99@users.noreply.github.com>
2022-07-29 00:49:14 +0530

    Create ch2ex1.cpp
    simple output using cout and explanations for it

---
+++ ch2ex1.cpp
@@ -0,0 +1,39 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout<<"C++ is better than C.\n";    // "" --> means string 
+
+    return 0;
+    
+        /*  
+        descriptions
+        
+        #include <bits/stdc++.h>  -->preprocessor directive to load inbuilt functions and we can use it
+        
+        in c++ main function always return an integer to show does the program run correctly or not
+        
+        so if program return 0 --> it means program run correctly
+        
+        other than 0 means there is some error in the program
+    
+        at cout<<variable
+        cout--> is an output object in std output strem
+        <<--> insertion or put operator it takes value of variable present in right of it and print/gives to screen
+        
+        
+        here variable can be int,float,double or string
+        
+        at cin>>variable  
+        
+        cout--> is an input object in std input strem
+        >> --> extraction  or get operator it takes value of variable present in left of it from keyboard
+        
+        here here variable can be int,float,double  or a single connected string like ramaya not ram aya--> space is not allowed 
+        we can use gets() or getline() for entering string with spaces
+    
+    */
+    
+}
