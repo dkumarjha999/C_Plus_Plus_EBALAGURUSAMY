d82eaad461f74a742532a208623a9e7104b478d1
Deepak kumar jha <44145322+kumardeepak99@users.noreply.github.com>
2022-07-30 01:11:16 +0530

    Create ch2exercise5.cpp
    converting fahrenheit into celsius

---
+++ ch2exercise5.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    float f,c;
+    cout<<"Enter temperature in fahrenheit: ";
+    cin>>f;
+    c=((f-32)*5)/9;
+    cout<<"temperature in celsius="<<c<<endl;
+    return 0;
+  
+  /*
+  
+  converting fahrenheit into celsius
+  
+  
+  we have relation / formula for this conversion which is c=((f-32)*5)/9;
+  
+  this relates fahrenheit and celsius so using one value we can find other value
+  
+  
+  */
+  
+}
