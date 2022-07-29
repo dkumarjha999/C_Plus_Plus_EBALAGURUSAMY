9f942fb6799b960f976a107d534d8fb82e7cef29
Deepak kumar jha <44145322+kumardeepak99@users.noreply.github.com>
2022-07-30 00:29:43 +0530

    Create ch2exercise1.cpp
    program to print in one line

---
+++ ch2exercise1.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int math=90,physics=77,chemistry=69;
+    cout<<"Maths=\t"<<math<<"\tPhysics=\t"<<physics<<"\tChemistry=\t"<<chemistry<<endl;
+
+    return 0;
+  
+  /*
+  
+  here all are integers so declared int variable for math,pysics & chemistry
+  
+  \t is used for tab space  and endl for end line same as "\n"
+  
+  */
+  
+}
