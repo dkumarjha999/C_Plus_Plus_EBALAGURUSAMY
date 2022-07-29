f2eedfc5cad074f8d9270ac9c88a4734339d7046
Deepak kumar jha <44145322+kumardeepak99@users.noreply.github.com>
2022-07-30 00:37:54 +0530

    Create ch2exercise2.cpp
    finding larger number in 2 numbers

---
+++ ch2exercise2.cpp
@@ -0,0 +1,31 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int num1,num2;
+    cout<<"Enter two numbers\n";
+    cin>>num1>>num2;
+    cout<<"larger numder is = ";
+    if(num1>num2) cout<<num1<<endl;
+    else cout<<num2<<endl;
+
+    return 0;
+  
+  /*
+  
+  here we are inputting 2 numbers in num1 & num2
+  
+  then using if condition we are finding which is bigger number
+  
+  if we use to check conditions and it gives boolean result true or false based on that it execute if or else
+  
+  it is not necessary we need to keep else always with if conditions also if we have multiple coditions we can use multiple if
+  
+  using else is our choice based on need.
+  
+  */
+  
+}
