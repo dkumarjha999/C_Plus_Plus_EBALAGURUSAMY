5c21210a8d4f399bf4b252bf6ecf56a5a579d7ac
Deepak kumar jha <44145322+kumardeepak99@users.noreply.github.com>
2022-07-30 00:46:26 +0530

    Create ch2exercise3.cpp
    printing WELL DONE n times

---
+++ ch2exercise3.cpp
@@ -0,0 +1,49 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter a number\n";
+    cin>>n;
+    
+    for(int i=0;i<n;i++){
+        cout<<"WELL DONE\n";
+    }
+    
+    // ohter way
+    
+    // while(n--){
+    //     cout<<"WELL DONE\n";
+    // }
+  
+  
+  /*
+  
+  this program is for printing WELL DONE n times
+  
+  for doing any work n number of thimes that is repeated work then we use loop
+  
+  we can use for loop
+  
+  which has 3 part 1) initialization here we initialize the iterating variable 2) condition for execution 3) increment or decrement
+  
+  here step 1 and 3 are optional but 2 must be there
+  
+  
+   or we can use while loop
+  
+  here based on one variable until condition in while is correct it executes
+  
+  or we can use DO while loop
+  
+  in do while loop first it executes once do condition then it goess for checking while condition for looping the do part again
+  
+  
+  */
+
+
+    return 0;
+}
