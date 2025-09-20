a878caf97e6848cf88ff40085f2fc0e822eaf751
dkumarjha999 <dkumarjha999@gmail.com>
2025-09-20 12:16:30 +0530

    Code cleanup and restructuring

--- prog0.cpp
+++ prog0.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+// this is a sample code template for c++ programming
+// you can modify it as per your requirement
 int main()
 {
     // write logic here
